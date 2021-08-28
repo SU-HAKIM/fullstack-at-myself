2eab0130797489289ba06469f9e9a75bd3f0dcd8
hakim <bmsuhakim@gmail.com>
2021-08-28 21:38:47 +0600

    seted up ejs and styles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 //?constants
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 //?instance route
 app.get("/", (req, res) => {
