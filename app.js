156ae212598d8223a17c6b006b3a1b7f741bbe85
hakim <bmsuhakim@gmail.com>
2021-08-28 22:12:13 +0600

    creating login controller

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
 //? external dependencies
 const express = require("express");
+const morgan = require('morgan');
+const authRoutes = require("./routes/authRouter");
+const mongoose = require("mongoose");
 
 //?constants
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
+
+//?view engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//?middleware
+const middleware = [
+    morgan('dev'),
+    express.json(),
+    express.urlencoded({ extended: true }),
+    express.static("public")
+]
+
+app.use(middleware);
+
+app.use('/auth', authRoutes)
 
 //?instance route
 app.get("/", (req, res) => {
+    res.render("pages/auth/signup", {
+        title: "create a new account",
+        error: { },
+        value: { },
+        isLoggedIn: false
+    })
     res.json({ message: "welcome to my app" })
 })
 
-//?listening to the server
-app.listen(PORT, () => {
-    console.log("listening to the port", PORT)
-})
+
+mongoose.connect('mongodb://localhost:27017/hakim',
+    {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log("DB connected")
+        //?listening to the server
+        app.listen(PORT, () => {
+            console.log("listening to the port", PORT)
+        })
+    }).catch(err => {
+        console.log(er)
+    });
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,48 @@
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
+
 exports.signupGetController = (req, res, next) => {
+    res.render("pages/auth/signup"), {
+        title: "Create A New Account",
+        error: { },
+        value: { },
+        isLoggedIn: req.session.isLoggedIn
+    }
 
 }
-exports.signupPostController = (req, res, next) => {
-
+exports.signupPostController = async (req, res, next) => {
+    let { username, email, password } = req.body;
+    try {
+        //? hashing password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        //?creating user
+        let user = new user({
+            username,
+            email,
+            password: hashedPassword
+        })
+        //? adding user
+        let createUser = await user.save();
+        console.log("Created user", createUser)
+        res.render('pages/auth/login',
+            {
+                error: { },
+                value: { },
+                isLoggedIn: req.session.isLoggedIn
+            }
+        )
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 }
 exports.loginGetController = (req, res, next) => {
-
+    res.render("pages/auth/login", {
+        title: "Log in to your account",
+        error: { },
+        value: { },
+        isLoggedIn: req.session.isLoggedIn
+    });
 }
 exports.loginPostController = (req, res, next) => {
 
